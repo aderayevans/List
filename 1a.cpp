9e0f76cf04e36c5429a22069d165a441062d8038
unknown <aderayevans@gmail.com>
2020-01-23 21:33:07 +0700

    finished 3

--- 1a.cpp
+++ 1a.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 #include "LIST1a.h"
 
+void ARRANGE_LIST(List &L) {
+    ElementType temp;
+    for (int index1 = 0; index1 < L.Last - 1; index1++) {
+        for (int index2 = index1 + 1; index2 < L.Last; index2++) {
+            if (L.Elements[index1] > L.Elements[index2]) {
+                temp = L.Elements[index1];
+                L.Elements[index1] = L.Elements[index2];
+                L.Elements[index2] = temp;
+            }
+        }
+    }
+}
 int main() {
     List L;
     MAKENULL_LIST(L);
     READ_LIST(L);
+    PRINT_LIST(L);
+    ARRANGE_LIST(L);
     PRINT_LIST(L);
 }
