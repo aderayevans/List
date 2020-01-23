8df5456d297eb59ea02efa4a162cc8650e4e9a21
unknown <aderayevans@gmail.com>
2020-01-23 16:08:13 +0700

    Fix 1b

--- LIST1b.h
+++ LIST1b.h
@@ -77,7 +77,7 @@ void READ_LIST(List &L) {
     for (int index = 0; index < amount; index++) {
         std::cout << "/<" << index + 1 << ">: ";
         std::cin >> x;
-        INSERT_LIST(x, 1, L);
+        INSERT_LIST(x, FIRST(L), L);
     }
 }
 void PRINT_LIST(List L) {
